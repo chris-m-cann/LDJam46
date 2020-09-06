490a4ceda7784c932847b999833ce830bfa38ebe
Chris Cann <canniman@googlemail.com>
2020-09-06 15:34:46 +0100

    make a dust prefab and start on the dynamic control prompts with the files added his time

---
+++ Assets/Scripts/CatBall/Controls/ControlActionUIElement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace CatBall.Controls
+{
+    public class ControlActionUIElement : MonoBehaviour
+    {
+        private CanvasRenderer _renderer;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<CanvasRenderer>();
+        }
+
+        // private void Start()
+        // {
+        //     if (_renderer != null)
+        //         _renderer.sor = "Background";
+        //     else
+        //     {
+        //         Debug.Log("no mesh renderer");
+        //     }
+        // }
+    }
+}
---
+++ Assets/Scripts/Util/UI/DisplayAction.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using CatBall;
+using TMPro;
+using UnityEngine;
+using Util.Control;
+
+namespace Util.UI
+{
+    public class DisplayAction : MonoBehaviour
+    {
+        [SerializeField] private PlayerAction action;
+        [SerializeField] private SchemeMappings kbMappings;
+        [SerializeField] private SchemeMappings controllerMappings;
+        [SerializeField] private InputDisplayMappings displayMappings;
+        [SerializeField] private TextMeshProUGUI text;
+        [SerializeField] private SpriteRenderer sprite;
+
+        private SchemeMappings _activeMappings;
+        private InputBindingsManager.ControlBinding? _binding;
+
+        private void Start()
+        {
+            _activeMappings = InputEx.IsControllerConnected() ? controllerMappings : kbMappings;
+            if (_activeMappings.CreateControlBindings().TryGetValue(action, out var binding))
+            {
+                _binding = binding;
+            }
+            UpdateText();
+        }
+
+        private void Update()
+        {
+            bool updateNeeded = false;
+
+            var mappings = InputEx.IsControllerConnected() ? controllerMappings : kbMappings;
+
+            if (_activeMappings.CreateControlBindings().TryGetValue(action, out var binding))
+            {
+                updateNeeded = _binding.HasValue && !Equals(_binding.Value, binding);
+                _binding = binding;
+            }
+
+            if (mappings != _activeMappings)
+            {
+                _activeMappings = mappings;
+                updateNeeded = true;
+            }
+
+            if (updateNeeded)
+            {
+                UpdateText();
+            }
+        }
+
+        private void UpdateText()
+        {
+            Debug.Log("Calling Update text");
+            if (_binding.HasValue)
+            {
+                var binding = _binding.Value;
+
+                if (binding.key != null)
+                {
+                    displayMappings.GetKeyCodeDisplay(binding.key.Value).Match(
+                        s =>
+                        {
+                            sprite.enabled = false;
+                            text.enabled = true;
+                            text.text = s;
+                        },
+                        img =>
+                        {
+                            text.enabled = false;
+                            sprite.enabled = true;
+                            sprite.sprite = img;
+                        }
+                        );
+                }
+                else
+                {
+                    displayMappings.GetAxisDisplay(binding.axis).Match(
+                        s =>
+                        {
+                            sprite.enabled = false;
+                            text.enabled = true;
+                            text.text = s;
+                        },
+                        img =>
+                        {
+                            text.enabled = false;
+                            sprite.enabled = true;
+                            sprite.sprite = img;
+                        }
+                    );
+                }
+            }
+            else
+            {
+                text.enabled = false;
+                sprite.enabled = false;
+            }
+        }
+    }
+}
